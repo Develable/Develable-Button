781bded3b3ab05e6c01c6182fd8fb1f1a7a8b7e5
HwaHyang <me@hwahyang.space>
2020-05-23 19:02:27 +0900

    20200523
    일부 코드 수정, 샘플 페이지 내용 일부 수정

--- dev05.js
+++ dev05.js
@@ -17,11 +17,20 @@ var loginButtons = document.getElementsByTagName("devlogin");
 for (var i = 0; i < loginButtons.length; i++) {
     var button = loginButtons.item(i);
     var link;
-    if($(button).attr("href")) link = $(button).attr("href");
-    else link = "javascript:void(0);";
+    if ($(button).attr("href")) {
+        link = $(button).attr("href");
+    } else {
+        link = "javascript:void(0);";
+    }
     $(button).each(function () {
-        if ($(this).hasClass("dev05-round") === true) $(this).html("<a class=\"dev05-base dev05-login dev05-round\" href=\"" + link + "\"><i class=\"dev05-ico-dev05\"></i><vr class=\"line\"></vr><span class=\"text\">Develable로 로그인</span></a>");
-        if ($(this).hasClass("dev05-square-round") === true) $(this).html("<a class=\"dev05-base dev05-login dev05-square-round\" href=\"" + link + "\"><i class=\"dev05-ico-dev05\"></i><vr class=\"line\"></vr><span class=\"text\">Develable로 로그인</span></a>");
-        if ($(this).hasClass("dev05-square") === true) $(this).html("<a class=\"dev05-base dev05-login dev05-square\" href=\"" + link + "\"><i class=\"dev05-ico-dev05\"></i><vr class=\"line\"></vr><span class=\"text\">Develable로 로그인</span></a>");
-    })
+        if ($(this).hasClass("dev05-round") === true) {
+            $(this).html("<a class=\"dev05-base dev05-login dev05-round\" href=\"" + link + "\"><i class=\"dev05-ico-dev05\"></i><vr class=\"line\"></vr><span class=\"text\">Develable로 로그인</span></a>");
+        }
+        if ($(this).hasClass("dev05-square-round") === true) {
+            $(this).html("<a class=\"dev05-base dev05-login dev05-square-round\" href=\"" + link + "\"><i class=\"dev05-ico-dev05\"></i><vr class=\"line\"></vr><span class=\"text\">Develable로 로그인</span></a>");
+        }
+        if ($(this).hasClass("dev05-square") === true) {
+            $(this).html("<a class=\"dev05-base dev05-login dev05-square\" href=\"" + link + "\"><i class=\"dev05-ico-dev05\"></i><vr class=\"line\"></vr><span class=\"text\">Develable로 로그인</span></a>");
+        }
+    });
 }
